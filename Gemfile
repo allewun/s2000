d76f37e37086933a4be4fbc972be2489d062bb63
Allen Wu <allen.wu@ucla.edu>
2015-02-21 11:45:45 -0800

    analyzing, working on filtering

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 ruby '2.2.0'
 
 group :development do
+  gem 'colorize'
   gem 'mechanize'
   gem 'sqlite3'
 end
--- analyze.rb
+++ analyze.rb
@@ -1,4 +1,7 @@
+require 'colorize'
 require 'sqlite3'
+
+
 
 def parse_content content
   # objective: get
@@ -11,15 +14,44 @@ def parse_content content
   #   color
   #   notes
 
+  year = nil
+  # match years, can be double digit (00-09)
+  #              or 4 digits (2000-2009)
+  #              and optionally preceded by "MY" (model year)
+  content.sub(/\b((my)?(20)?0\d)\b/i) { year = $1 }
 
+  if year
+    p year
+  else
+    p content
+  end
+
+end
+
+# filters
+
+def not_enough_numbers content
+  content.scan(/\d/).count < 6
+end
+
+def too_many_questions content
+  content.scan('? ').count > 1
 end
 
 
 db = SQLite3::Database.new 's2ki.db'
 
-db.execute('SELECT * FROM forum') do |row|
+rows = db.execute('SELECT * FROM forum')
+
+rows.reject! do |*, content|
+  should_reject = not_enough_numbers(content) || too_many_questions(content)
+  puts "Filtered: #{content}".red if should_reject
+  should_reject
+end
+
+rows.each do |row|
   username, published, location, content = row
 
-  parse_content content
+  parse_content content if content
 end
 
