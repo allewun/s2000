661ee6c4ec3ca13d4bf0bd73a27c9a57f2c33cc3
Allen Wu <allen.wu@ucla.edu>
2015-02-20 20:49:19 -0800

    Added gemfile

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+ruby '2.2.0'
+
+group :development do
+  gem 'mechanize'
+  gem 'sqlite3'
+end
--- scrape.rb
+++ scrape.rb
@@ -1,1 +1,21 @@
 require 'mechanize'
+require 'sqlite3'
+
+db = SQLite3::Database.new 's2ki.db'
+
+rows = db.execute <<-SQL
+  create table s2000raw (
+    year     integer
+    mileage  integer
+    price    integer
+    purchase text
+    location text
+    cr       integer
+    color    text
+    notes    text
+    username text
+  )
+SQL
+
+
+m = Mechanize.new
