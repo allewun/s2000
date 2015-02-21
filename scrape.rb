ef2f94479910676d4765696fbee119a1f3f586fe
Allen Wu <allen.wu@ucla.edu>
2015-02-20 23:39:57 -0800

    Ready to scrape

--- scrape.rb
+++ scrape.rb
@@ -1,43 +1,55 @@
 require 'mechanize'
 require 'sqlite3'
 
-# db = SQLite3::Database.new 's2ki.db'
+puts "Starting S2000 data collection...\n"
 
-# rows = db.execute <<-SQL
-#   create table forum (
-#     username text
-#     publised text
-#     location text
-#     content  text
-#   );
+# setup database
+puts "Setting up database...\n"
+db = SQLite3::Database.new 's2ki.db'
 
-#   create table s2000 (
-#     year     integer
-#     mileage  integer
-#     price    integer
-#     purchase text
-#     location text
-#     cr       integer
-#     color    text
-#     notes    text
-#     username text
-#   );
-# SQL
+db.execute <<-SQL
+  CREATE TABLE forum (
+    username  TEXT,
+    published TEXT,
+    location  TEXT,
+    content   TEXT
+  );
 
+  CREATE TABLE s2000 (
+    year     INTEGER,
+    mileage  INTEGER,
+    price    INTEGER,
+    purchase TEXT,
+    location TEXT,
+    cr       INTEGER,
+    color    TEXT,
+    notes    TEXT,
+    username TEXT
+  );
+SQL
+
+
+# begin scraping
+puts "Beginning to scrape...\n"
 m = Mechanize.new
-page = m.get('http://www.s2ki.com/s2000/topic/903878-how-much-did-you-pay-for-your-used-s2000/')
 
-# get posts that don't contain quotes
-page.search('.//div[contains(@id, "post_id_")]/div[@class="post_wrap"][not(descendant::div[@class="quote"])]').each do |post|
-  published = post.search('abbr.published').attribute('title').text
-  author    = post.search('span.author.vcard > a').text
-  location  = post.search('.//li[span[@class="ft"]/text() = "Location:"]/span[@class="fc"]').text
-  content   = post.search('div.entry-content').text.gsub(/^\s+|\s+$/, '')
+link = 'http://www.s2ki.com/s2000/topic/903878-how-much-did-you-pay-for-your-used-s2000/'
 
-  data = {published: published,
-          author: author,
-          location: location,
-          content: content}
+(1..10000).each do |i|
+  puts "Fetching page #{i}...\n"
+  page = m.get(link)
+  link = page.search('li.next > a[title="Next page"]').attribute("href").text rescue nil
 
-  pp data
+  # get posts that don't contain quotes
+  page.search('.//div[contains(@id, "post_id_")]/div[@class="post_wrap"][not(descendant::div[@class="quote"])]').each do |post|
+    published = post.search('abbr.published').attribute('title').text
+    author    = post.search('span.author.vcard > a').text
+    location  = post.search('.//li[span[@class="ft"]/text() = "Location:"]/span[@class="fc"]').text
+    content   = post.search('div.entry-content').text.gsub(/^\s+|\s+$/, '')
+
+    db.execute("INSERT INTO forum (username, published, location, content)
+                VALUES (?, ?, ?, ?)", [author, published, location, content])
+  end
+
+  break unless link
 end
